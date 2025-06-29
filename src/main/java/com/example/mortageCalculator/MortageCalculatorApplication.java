242611489124cba69ea145212f821ae96339f35d
lukaslaifr <111529208+TheLimetka@users.noreply.github.com>
2025-06-29 09:33:13 +0200

    First comm

---
+++ src/main/java/com/example/mortageCalculator/MortageCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mortageCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortageCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortageCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mortageCalculator/MortageCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mortageCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortageCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
